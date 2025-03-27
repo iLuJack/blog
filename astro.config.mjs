0a2e2c5230dfd519aa54d547001a34eb6390a656
iLuJack <jack112liu@gmail.com>
2025-03-27 21:15:18 +0800

    test
    fix test format
    
    post should follow certain format

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://iLuJack.github.io',
+  site: 'https://fuwari.vercel.app/',
   base: '/',
   trailingSlash: "always",
   integrations: [
