406f11acb893c8afa00ffff97151943a559314fc
iLuJack <jack112liu@gmail.com>
2025-03-27 21:39:45 +0800

    vercel-config

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,12 +18,14 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
-
+import { vercel } from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
+  site: 'https://ilujack.vercel.app/',
   base: '/',
   trailingSlash: "always",
+  output: "server",
+  adapter: vercel(),
   integrations: [
     tailwind(
         {
