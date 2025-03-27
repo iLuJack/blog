f891fbe284784d5b4e0dc744854988e8b83708ca
iLuJack <jack112liu@gmail.com>
2025-03-27 20:12:40 +0800

    vercel deployment test

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,10 +18,12 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
-
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  // output: "server",
+  // adapter: vercel(),
+  // site: "https://fuwari.vercel.app/", my vercel app
   base: "/",
   trailingSlash: "always",
   integrations: [
---
+++ src/content/posts/test.md
@@ -0,0 +1,10 @@
+---
+title: test
+published: 2025-03-27
+description: ''
+image: ''
+tags: []
+category: ''
+draft: false 
+lang: ''
+---
